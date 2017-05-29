ba69d56862df793588814f361100de9e01ad2fac
lukebelliveau <lbellive@thoughtworks.com>
2017-05-29 16:07:18 -0400

    binary tree working with drawing lines, need to add delete op

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// <animate attributeType="XML" attributeName="cx" from="-100" to="120"
-// dur="2s" repeatCount="indefinite"/>
-
 import BinaryTree, { TreeNode } from './binaryTree/BinaryTree';
 
 const style = {
   div: {
-    height: '500px',
+    height: '400px',
     width: '400px',
   },
   circle: {
@@ -17,83 +14,105 @@ const style = {
     strokeWidth: '.5'
   },
   text: {
-    fontSize: 7,
+    fontSize: 5,
   }
 }
 
-const TreeBox = ({ children }) => {
-  // const center = {x:50, y:50}
-  // const radius = 5
-  // const node = new TreeNode(5);
-
-  return (
-    <div style={ style.div }>
-      <svg viewBox="0 0 100 100">
-        { children }
-      </svg>
-    </div>
-  )
-};
+const radius = 3;
+const mockY = 50;
 
 const getCenter = (lowerBound, upperBound) => {
   return (upperBound-lowerBound)/2 + lowerBound
 }
 
-const NodeComponent = ({ node, x }) => {
-  const radius = 5;
+const NodeComponent = ({ node, x, y }) => (
+    <svg>
+      <circle cx={ x } cy={ y } r={radius} style={ style.circle }>
+      </circle>
+      <text x={ x } y={ y + 2 } textAnchor="middle" style={ style.text }>
+        {node.data}
+      </text>
+    </svg>
+);
 
-  const y = 50;
+const getChildY = (parentCenter) => (parentCenter ? parentCenter.y : 0) + (radius * 4);
+
+const RecursiveNodes = ({ node, leftBound, rightBound, parentCenter }) => {
+  if (!node) return null;
+
+  const x = getCenter(leftBound, rightBound);
+  const y = getChildY(parentCenter);
+  const leftChildren = node.leftChild ?
+    [
+      <line x1={ x - radius } y1={ y } x2={ getCenter(leftBound, x) } y2={ getChildY({x, y}) } stroke-width="1" stroke="black"/>,
+      <RecursiveNodes node={ node.leftChild } leftBound={ leftBound } rightBound={ x } parentCenter={{ x, y }}  />,
+    ]
+    : null;
+  const rightChildren = node.rightChild ?
+    [
+      <line x1={ x + radius } y1={ y } x2={ getCenter(x, rightBound) } y2={ getChildY({x, y}) } stroke-width="1" stroke="black"/>,
+      <RecursiveNodes node={ node.rightChild } leftBound={ x } rightBound={ rightBound } parentCenter={{ x, y }} />
+    ]
+    : null;
 
   return (
-      <svg>
-        <circle cx={ x } cy={ y } r={radius} style={ style.circle }>
-        </circle>
-        <text x={ x } y={ y + 2.5 } textAnchor="middle" style={ style.text }>
-          {node.data}
-        </text>
-      </svg>
+    <svg>
+      <NodeComponent x={ x } y={ y } leftBound={ leftBound } rightBound={ rightBound } node={ node }/>
+      { leftChildren }
+      { rightChildren }
+    </svg>
   )
 }
 
-const tree = new BinaryTree();
-tree.insert(new TreeNode(5));
-tree.insert(new TreeNode(4));
-tree.insert(new TreeNode(3));
-tree.insert(new TreeNode(6));
+const NodeMaker = ({ enteredValue, onInputChange, onButtonClick }) => {
+  const buttonText = enteredValue === '' ?
+                      `Enter some text to create a node!` :
+                      `Click here to create a node with ${ typeof enteredValue } ${ enteredValue }`
 
-const rootNode = tree.root;
-
-const RecursiveNodes = ({ node, leftBound, rightBound }) => {
-  const center = getCenter(leftBound, rightBound);
-  const leftChildren = node.leftChild ? <RecursiveNodes node={ node.leftChild } leftBound={ leftBound } rightBound={ center } /> : null;
-  const rightChildren = node.rightChild ? <RecursiveNodes node={ node.rightChild } leftBound={ center } rightBound={ rightBound } /> : null;
-
-  if(node.leftChild) {
-    return (
-      <svg>
-        <NodeComponent x={ center } leftBound={ leftBound } rightBound={ rightBound } node={ node }/>
-        { leftChildren }
-        { rightChildren }
-      </svg>
-    )
-  }
-  else return (
-    <NodeComponent x={ center } leftBound={ leftBound } rightBound={ rightBound } node={ node }/>
-  );
-}
-
-
-const App = ({ root }) => {
   return (
     <div>
-    <TreeBox>
-      <RecursiveNodes node={ root } leftBound={ 0 } rightBound={ 100 }/>
-    </TreeBox>
+      <input placeholder="Enter a value" value={ enteredValue } onChange={ onInputChange }></input>
+      <button onClick={ onButtonClick }>{ buttonText }</button>
     </div>
   )
 };
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enteredValue: '',
+      tree: new BinaryTree(),
+    }
+
+    this.changeEnteredValue = this.changeEnteredValue.bind(this);
+    this.createNode = this.createNode.bind(this);
+  };
+
+  changeEnteredValue(event) {
+    let value = event.target.value;
+    if(!isNaN(value) && value.length > 0) value = parseInt(value);
+    this.setState(() => ({enteredValue: value}))
+  };
+
+  createNode() {
+    this.state.tree.insert(new TreeNode(this.state.enteredValue));
+    this.setState(() => ({enteredValue: ''}))
+  }
+
+  render() {
+    return (
+      <div style={ style.div }>
+        <svg viewBox="0 0 100 100">
+          <RecursiveNodes node={ this.state.tree.root } leftBound={ 0 } rightBound={ 100 } parentY={ -5 }/>
+        </svg>
+        <NodeMaker enteredValue={ this.state.enteredValue } onInputChange={ this.changeEnteredValue } onButtonClick={ this.createNode }/>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <App root={ rootNode }/>,
+  <App />,
   document.getElementById('root')
 );
